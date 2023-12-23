aecf05cc8eb9c1b46372714251c1891f5df126a8
DhruvDutta <dhrvdutta@gmail.com>
2023-12-23 22:00:22 +0530

    filter working

--- main/filters.py
+++ main/filters.py
@@ -12,10 +12,10 @@ class BoxFilter(filters.FilterSet):
     breadth__lt = filters.NumberFilter(field_name='breadth', lookup_expr='lt')
     height__gt = filters.NumberFilter(field_name='height', lookup_expr='gt')
     height__lt = filters.NumberFilter(field_name='height', lookup_expr='lt')
-    area__gt = filters.NumberFilter(method='filter_area_gt')
-    area__lt = filters.NumberFilter(method='filter_area_lt')
-    volume__gt = filters.NumberFilter(method='filter_volume_gt')
-    volume__lt = filters.NumberFilter(method='filter_volume_lt')
+    area__gt = filters.NumberFilter(field_name='area', lookup_expr='gt')
+    area__lt = filters.NumberFilter(field_name='area', lookup_expr='lt')
+    volume__gt = filters.NumberFilter(field_name='volume', lookup_expr='gt')
+    volume__lt = filters.NumberFilter(field_name='volume', lookup_expr='lt')
     created_at__gt = filters.DateTimeFilter(
         field_name='created_at', lookup_expr='gt')
     created_at__lt = filters.DateTimeFilter(
@@ -27,19 +27,3 @@ class BoxFilter(filters.FilterSet):
     class Meta:
         model = Box
         fields = ['length', 'breadth', 'height', 'created_at']
-
-    def filter_area_gt(self, queryset, name, value):
-        return queryset.filter(length__gt=0, breadth__gt=0).annotate(
-            area=F('length') * F('breadth')).filter(area__gt=value)
-
-    def filter_area_lt(self, queryset, name, value):
-        return queryset.filter(length__gt=0, breadth__gt=0).annotate(
-            area=F('length') * F('breadth')).filter(area__lt=value)
-
-    def filter_volume_gt(self, queryset, name, value):
-        return queryset.filter(length__gt=0, breadth__gt=0, height__gt=0).annotate(
-            volume=F('length') * F('breadth') * F('height')).filter(volume__gt=value)
-
-    def filter_volume_lt(self, queryset, name, value):
-        return queryset.filter(length__gt=0, breadth__gt=0, height__gt=0).annotate(
-            volume=F('length') * F('breadth') * F('height')).filter(volume__lt=value)
